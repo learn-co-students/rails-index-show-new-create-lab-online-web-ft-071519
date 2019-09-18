0412e61b58d002f2fdb038104e8abcc8075f4d1f
Nicole Dow <nikki.dow71@gmail.com>
2019-09-18 09:58:19 -0400

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -12,4 +12,8 @@ class CouponsController < ApplicationController
     def index
         @coupons = Coupon.all
     end
+
+    def show
+      @coupon = Coupon.find_by_id(params["id"])
+    end
 end
